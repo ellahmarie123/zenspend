377cc744dc17f740700809f94c8ceeaad1849d07
Tatualia <tatualia.es@pg.com>
2025-07-23 20:38:50 +0800

    Updated the supabaseClient file for creds to not be hardcoded

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://vbigmbuprkfmltyqempg.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaWdtYnVwcmtmbWx0eXFlbXBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyNjgyNDIsImV4cCI6MjA2ODg0NDI0Mn0.Oysec2dtCKwAcS17a6MTMgoFG3aXb90NFqWAKjJHqrg";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
